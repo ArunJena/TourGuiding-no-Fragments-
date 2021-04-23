96265e5dd53e85448133c498fd7908ba79a70f97
Arun <arunjena632@gmail.com>
2021-04-23 20:22:56 +0530

    Launcher Activity done

---
+++ app/src/main/java/com/example/tourgiuiding2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tourgiuiding2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void intentTemple(View v){
+        Intent intent = new Intent(getApplicationContext(),Temple.class);
+        startActivity(intent);
+    }
+    public void intentHotel(View v){
+        Intent intent = new Intent(getApplicationContext(),Hotel.class);
+        startActivity(intent);
+    }
+    public void intentSites(View v){
+        Intent intent = new Intent(getApplicationContext(),Sites.class);
+        startActivity(intent);
+    }
+}
